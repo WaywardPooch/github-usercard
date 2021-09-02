009671786309acfd57ba54488cdf6e9677f7b0d6
Lance Turbes <waywardpooch@users.noreply.github.com>
2021-09-02 15:48:50 -0700

    refactored/cleaned code for clarity; added comments

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,52 +2,13 @@
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
-*/
 
-const entryPoint = document.querySelector("div.cards");
-
-const addGitHubUserCard = async (username) => {
-  try {
-    const response = await axios.get(
-      `https://api.github.com/users/${username}`
-    );
-    entryPoint.appendChild(makeUserCard(response.data));
-  } catch (error) {
-    console.log("Error fetching user data!");
-  } finally {
-    console.log("getGitHubUserData() was run!");
-  }
-};
-
-addGitHubUserCard("WaywardPooch");
-
-/*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
 
     Skip to STEP 3 (line 34).
-*/
 
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
-/*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
@@ -65,9 +26,39 @@ const followersArray = [];
         <p>Bio: {users bio}</p>
       </div>
     </div>
+
+  STEP 4: Pass the data received from Github into your function,
+    and append the returned markup to the DOM as a child of .cards
+
+  STEP 5: Now that you have your own card getting added to the DOM, either
+    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
+    manually find some other users' github handles, or use the list found at the
+    bottom of the page. Get at least 5 different Github usernames and add them as
+    Individual strings to the friendsArray below.
+
+    Using that array, iterate over it, requesting data for each user, creating a new card for each
+    user, and adding that card to the DOM.
+
+  List of LS Instructors Github username's:
+    tetondan
+    dustinmyers
+    justsml
+    luishrd
+    bigknell
 */
 
-function makeUserCard(userObj) {
+// Declare Variables
+const entryPoint = document.querySelector("div.cards");
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell",
+];
+
+// Declare Functions
+const makeUserCard = (userObj) => {
   // Create elements
   const userCard = document.createElement("div");
   const userImage = document.createElement("img");
@@ -114,13 +105,29 @@ function makeUserCard(userObj) {
 
   // Return the created markup
   return userCard;
-}
+};
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+const addGitHubUserCard = async (username) => {
+  try {
+    // Get the user's information from github
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+    // Create/append user card, with info received, to the page
+    entryPoint.appendChild(makeUserCard(response.data));
+  } catch (error) {
+    // If the data cannot be fetched, log an error message
+    console.log("Error fetching user data!");
+  } finally {
+    // Whenever this function is run, log a success message
+    console.log("getGitHubUserData() was run!");
+  }
+};
+
+// Add my own user card to the page
+addGitHubUserCard("WaywardPooch");
+
+// Add "follower" user cards to the page
+followersArray.forEach((follower) => {
+  addGitHubUserCard(follower);
+});
